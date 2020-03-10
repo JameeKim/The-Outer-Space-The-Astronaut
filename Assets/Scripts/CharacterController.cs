bc034ed336484a66f4f7015f1f83a753e82a749c
Jamee Kim <jameekim@nodaplife.me>
2020-03-09 20:35:06 -0400

    Add background placeholder and player character

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class CharacterController : MonoBehaviour
+{
+    public float maxSpeed = 10.0f;
+
+    private Rigidbody2D rigidBody;
+
+    private void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        float horiz = Input.GetAxis("Horizontal");
+        float vert = Input.GetAxis("Vertical");
+        Vector2 directionRaw = new Vector2(horiz, vert);
+        Vector2 velocity = directionRaw.magnitude * maxSpeed * directionRaw.normalized;
+
+        rigidBody.velocity = velocity;
+    }
+}
