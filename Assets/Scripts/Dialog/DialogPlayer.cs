a67b3db72b5a17708d967aca460e060fafe37482
Jamee Kim <jameekim@nodaplife.me>
2020-04-06 04:31:02 -0400

    Create `DialogPlayer` as a way to trigger a dialog play

--- Assets/Scripts/Dialog/DialogPlayer.cs
+++ Assets/Scripts/Dialog/DialogPlayer.cs
@@ -1,15 +1,33 @@
-﻿using UnityEngine;
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
 
 namespace Dialog {
     public class DialogPlayer : MonoBehaviour
     {
-        public DialogSeries tmpDialogList;
+        [InfoBox("This component is meant to be used as a way to register a callback to play a dialog. "
+            + "Expose a `UnityEvent` public field on the desired component, and register the `PlayDialog` method "
+            + "of this component with the wanted dialog list asset as the argument. "
+            + "Then, invoke that `UnityEvent` in the code of the component in the desired places.")]
+        [SceneObjectsOnly]
+        [ValidateInput("MustBeSet")]
         public DialogPresenter dialogPresenter;
 
-        public void PlayDialog()
+        public void PlayDialog(DialogSeries dialogList)
         {
             dialogPresenter.gameObject.SetActive(true);
-            dialogPresenter.StartDialog(tmpDialogList);
+            dialogPresenter.StartDialog(dialogList);
         }
+
+#if UNITY_EDITOR
+        private bool MustBeSet(DialogPresenter value, ref string errorMsg)
+        {
+            bool isValid = value != null;
+            if (!isValid)
+            {
+                errorMsg = "The field must be set to a non-null value";
+            }
+            return isValid;
+        }
+#endif
     }
 }
