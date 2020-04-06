e49a21b5f093140e29e09e61363823ae91339e33
Jamee Kim <jameekim@nodaplife.me>
2020-04-06 06:43:54 -0400

    Add player character sprites and animation

---
+++ Assets/Scripts/PlayerCharacter/PlayerCharacterAnimator.cs
@@ -0,0 +1,50 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace PlayerCharacter {
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class PlayerCharacterAnimator : MonoBehaviour
+    {
+        [SceneObjectsOnly]
+        [ValidateInput("MustBeSet")]
+        public Animator spriteAnimator;
+
+        private Rigidbody2D rigidBody;
+
+        private static readonly int horizontal = Animator.StringToHash("Horizontal");
+        private static readonly int speed = Animator.StringToHash("Speed");
+        private static readonly int hurt = Animator.StringToHash("Hurt");
+
+        /// <summary>
+        ///   <para>Triggers the animation for the character being hurt.</para>
+        /// </summary>
+        public void GetHurt()
+        {
+            spriteAnimator.SetTrigger(hurt);
+        }
+
+        private void Start()
+        {
+            rigidBody = GetComponent<Rigidbody2D>();
+        }
+
+        private void FixedUpdate()
+        {
+            Vector2 currentVelocity = rigidBody.velocity;
+            spriteAnimator.SetFloat(horizontal, currentVelocity.x);
+            spriteAnimator.SetFloat(speed, currentVelocity.magnitude);
+        }
+
+#if UNITY_EDITOR
+        private bool MustBeSet(Animator value, ref string errorMsg)
+        {
+            bool isValid = value != null;
+            if (!isValid)
+            {
+                errorMsg = "The field must be set to a non-null value";
+            }
+            return isValid;
+        }
+#endif
+    }
+}
---
+++ Assets/Scripts/PlayerCharacter/PlayerCharacterController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace PlayerCharacter {
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class PlayerCharacterController : MonoBehaviour
+    {
+        [Min(0.0f)]
+        public float maxSpeed = 10.0f;
+
+        private Rigidbody2D rigidBody;
+
+        private void Start()
+        {
+            rigidBody = GetComponent<Rigidbody2D>();
+        }
+
+        private void FixedUpdate()
+        {
+            float horiz = Input.GetAxis("Horizontal");
+            float vert = Input.GetAxis("Vertical");
+
+            // Map the square range of {(x,y) | -1 <= x,y <= 1} into unit circle {(x,y) | x^2 + y^2 <= 1}
+            float normalizingFactor = Mathf.Max(Mathf.Abs(horiz), Mathf.Abs(vert));
+            Vector2 direction= new Vector2(horiz, vert).normalized;
+            Vector2 velocity = normalizingFactor * maxSpeed * direction;
+
+            rigidBody.velocity = velocity;
+        }
+    }
+}
