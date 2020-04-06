2810bfd033673d57ec240bd5f2a1ed303931b5f8
Jamee Kim <jameekim@nodaplife.me>
2020-04-06 16:57:48 -0400

    Implement dialog system (#18)
    * Implement dialog functionality
    
    * Create `DialogPlayer` as a way to trigger a dialog play
    
    * Fix all rebase issues
    
    * Pause game when playing dialog
    
    * Automatically disable dialog canvas

---
+++ Assets/Scripts/Dialog/DialogPlayer.cs
@@ -0,0 +1,49 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Dialog {
+    public class DialogPlayer : MonoBehaviour
+    {
+        [InfoBox("This component is meant to be used as a way to register a callback to play a dialog. "
+            + "Expose a `UnityEvent` public field on the desired component, and register the `PlayDialog` method "
+            + "of this component with the wanted dialog list asset as the argument. "
+            + "Then, invoke that `UnityEvent` in the code of the component in the desired places.")]
+        [SceneObjectsOnly]
+        [ValidateInput("MustBeSet")]
+        public DialogPresenter dialogPresenter;
+
+        public void PlayDialog(DialogSeries dialogList)
+        {
+            dialogPresenter.gameObject.SetActive(true);
+            dialogPresenter.StartDialog(dialogList);
+        }
+
+        /// <summary>
+        ///   <para>Pause the game.</para>
+        /// </summary>
+        public void StartDialog()
+        {
+            Time.timeScale = 0.0f;
+        }
+
+        /// <summary>
+        ///   <para>Resume the paused game.</para>
+        /// </summary>
+        public void EndDialog()
+        {
+            Time.timeScale = 1.0f;
+        }
+
+#if UNITY_EDITOR
+        private bool MustBeSet(DialogPresenter value, ref string errorMsg)
+        {
+            bool isValid = value != null;
+            if (!isValid)
+            {
+                errorMsg = "The field must be set to a non-null value";
+            }
+            return isValid;
+        }
+#endif
+    }
+}
---
+++ Assets/Scripts/Dialog/DialogSeries.cs
@@ -0,0 +1,11 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Dialog {
+    [CreateAssetMenu(fileName = "NewDialogList", menuName = "New Dialog List", order = 5)]
+    public class DialogSeries : ScriptableObject
+    {
+        [ListDrawerSettings(Expanded = true)]
+        public DialogItem[] dialogItems;
+    }
+}
