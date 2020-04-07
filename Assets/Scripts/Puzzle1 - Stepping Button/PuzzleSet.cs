5f036673d45c22f2de04b4260cf22478916b292a
Eric <eric.srna@gmail.com>
2020-04-06 21:23:25 -0400

    Add Stepping Puzzle Prototype

---
+++ Assets/Scripts/Puzzle1 - Stepping Button/PuzzleSet.cs
@@ -0,0 +1,13 @@
+﻿
+public class PuzzleSet
+{
+    public static int[] puzzleAnswer = new int[4] { 1, 2, 3, 4 };
+    public static bool isEqual;
+    public static int seq;
+
+    public PuzzleSet()
+    {
+        isEqual = false;
+        seq = 0;
+    }
+}
---
+++ Assets/Scripts/Puzzle1 - Stepping Button/SteppingButton.cs
@@ -0,0 +1,62 @@
+﻿using Sirenix.OdinInspector;
+using System;
+using UnityEngine;
+
+public class SteppingButton : MonoBehaviour
+{
+    [Title("Status")]
+    [Tooltip("If this is currently able to be solved or not")]
+    public bool isActive = true;
+
+    [Title("Stepping Order")]
+    [Tooltip("What is an order of this button of the puzzle")]
+    public int steppingOrder;
+
+    private SpriteRenderer spriteRenderer;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+        
+    public void Stepping(GameObject player)
+    {
+        if (isActive)
+        {
+            CheckingAnswer(steppingOrder);
+        }
+    }
+
+    public void CheckingAnswer(int buttonOrder)
+    {
+        if (!OnPuzzle.isEqual)
+        {
+            if (buttonOrder == OnPuzzle.puzzleAnswer[OnPuzzle.seq])
+            {
+                OnPuzzle.seq = OnPuzzle.seq +1;
+                isActive = false;
+                spriteRenderer.sprite = null; // get rid of the psrite of hide from the user
+
+                // feedback of the success
+                Debug.Log("Checking Puzzle Answer : matched an order of the puzzle");
+            } else
+            {
+                // feedback of the failure
+                spriteRenderer.color = new Color(255, 0, 0);
+                Debug.Log("Checking Puzzle Answer : mismatched an order of the puzzle");
+            }
+
+            if (OnPuzzle.seq == OnPuzzle.puzzleAnswer.Length)
+            {
+                OnPuzzle.isEqual = true;
+
+                // rewards of solving the puzzle
+                Debug.Log("Congratulation. Puzzle is solved. Go forward.");
+            }
+        }
+    }
+
+    [Serializable]
+    public class OnPuzzle : PuzzleSet {}
+}
