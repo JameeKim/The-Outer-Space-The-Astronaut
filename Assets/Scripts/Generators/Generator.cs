4753aae1ff67b70528649ffebcc12031fc8e1294
Jamee Kim <jameekim@nodaplife.me>
2020-04-09 15:19:19 -0400

    Create generators

---
+++ Assets/Scripts/Generators/Generator.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Generators {
+    public class Generator : MonoBehaviour
+    {
+        public UnityEvent onActivated;
+
+        private bool isActivated;
+
+        public void Activate()
+        {
+            if (isActivated)
+                return;
+
+            isActivated = true;
+            onActivated.Invoke();
+        }
+    }
+}
---
+++ Assets/Scripts/Generators/SpriteSwapper.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Generators {
+    [DisallowMultipleComponent]
+    [RequireComponent(typeof(SpriteRenderer))]
+    public class SpriteSwapper : MonoBehaviour
+    {
+        public Sprite spriteToSwap;
+
+        private SpriteRenderer spriteRenderer;
+
+        private void Start()
+        {
+            spriteRenderer = GetComponent<SpriteRenderer>();
+        }
+
+        public void SwapSprite()
+        {
+            spriteRenderer.sprite = spriteToSwap;
+        }
+    }
+}
