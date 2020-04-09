ab72e71b029d4902bbead08975e22916c824999b
Jamee Kim <jameekim@nodaplife.me>
2020-04-09 09:58:05 -0400

    Create trap and implement it

--- Assets/Scripts/SelfFadeOut.cs
+++ Assets/Scripts/SelfFadeOut.cs
@@ -4,7 +4,15 @@ using UnityEngine;
 [RequireComponent(typeof(SpriteRenderer))]
 public class SelfFadeOut : MonoBehaviour
 {
+    [Tooltip("If fade out should start automatically")]
+    public bool autoStart = true;
+
+    [Tooltip("Delay from trigger to the start of fade out")]
+    [Min(0.0f)]
     public float offset = 1.0f;
+
+    [Tooltip("Duration of the fade out")]
+    [Min(0.01f)]
     public float duration = 0.5f;
 
     private SpriteRenderer spriteRenderer;
@@ -17,14 +25,21 @@ public class SelfFadeOut : MonoBehaviour
         maybeCollider = GetComponent<Collider2D>();
         maybeRigidBody = GetComponent<Rigidbody2D>();
 
+        if (autoStart)
+            StartCoroutine(FadeOutCoroutine());
+    }
+
+    public void StartFadeOut()
+    {
         StartCoroutine(FadeOutCoroutine());
     }
 
     private IEnumerator FadeOutCoroutine()
     {
-        yield return new WaitForSeconds(offset);
+        if (offset > 0.0f)
+            yield return new WaitForSeconds(offset);
 
-        if (maybeCollider != null) Destroy(maybeCollider);
+        if (maybeCollider != null) maybeCollider.enabled = false;
         if (maybeRigidBody != null) Destroy(maybeRigidBody);
 
         float time = 0.0f;
---
+++ Assets/Scripts/Trap/PlayerDamager.cs
@@ -0,0 +1,32 @@
+﻿using Combat;
+using PlayerCharacter;
+using UnityEngine;
+
+namespace Trap {
+    public class PlayerDamager : MonoBehaviour
+    {
+        public int damage = 10;
+        public float paralyzeDuration = 0.5f;
+
+        public void DoDamage(GameObject player)
+        {
+            // Assume that all of these components are present because we checked if the game object is the player.
+            // There is only one player character, and we rely on setting that game object correctly.
+            CombatEntity combatEntity = player.GetComponent<CombatEntity>();
+            PlayerCharacterAnimator animator = player.GetComponent<PlayerCharacterAnimator>();
+
+            combatEntity.GetAttacked(damage); // do damage
+            animator.GetHurt(); // play the animation to indicate the player took damage
+
+            if (paralyzeDuration <= 0.0f)
+                return; // all things done for damagers that do not paralyze the player
+
+            // Same as above, assume all of these components are there.
+            Rigidbody2D rigidBody = player.GetComponent<Rigidbody2D>();
+            PlayerCharacterController controller = player.GetComponent<PlayerCharacterController>();
+
+            rigidBody.velocity = Vector2.zero; // stop the player character
+            controller.DisableInputForSeconds(paralyzeDuration); // disable input for stalling effect
+        }
+    }
+}
