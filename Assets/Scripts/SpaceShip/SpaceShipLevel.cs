8c721d99bd869166b63ce1949e8976c5bb266b98
Jamee Kim <jameekim@nodaplife.me>
2020-04-09 14:52:16 -0400

    Have a space ship that indicates the charge level

---
+++ Assets/Scripts/SpaceShip/SpaceShipLevel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace SpaceShip {
+    public class SpaceShipLevel : MonoBehaviour
+    {
+        [InlineEditor(InlineEditorObjectFieldModes.Hidden)]
+        [FoldoutGroup("Settings")]
+        public SpaceShipSettings settings;
+
+        public LevelUpEvent onLevelUp;
+
+        private int currentLevel;
+
+        public int CurrentLevel => currentLevel;
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+                LevelUp();
+        }
+
+        public void LevelUp()
+        {
+            if (currentLevel == settings.MaxLevel)
+            {
+                Debug.LogError("Tried to level up when the current level is at its maximum");
+                return;
+            }
+
+            currentLevel++;
+            onLevelUp.Invoke(currentLevel);
+        }
+
+        public void OnPlayerCollide(GameObject player)
+        {
+            if (currentLevel < settings.MaxLevel)
+                return;
+
+            Debug.Log("The game should end here"); // TODO play game finished scene
+        }
+
+        [Serializable]
+        public class LevelUpEvent : UnityEvent<int> {}
+    }
+}
---
+++ Assets/Scripts/SpaceShip/SpaceShipStatus.cs
@@ -0,0 +1,26 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace SpaceShip {
+    [DisallowMultipleComponent]
+    [RequireComponent(typeof(SpriteRenderer))]
+    public class SpaceShipStatus : MonoBehaviour
+    {
+        [InlineEditor(InlineEditorObjectFieldModes.Hidden)]
+        [FoldoutGroup("Settings")]
+        public SpaceShipSettings settings;
+
+        private SpriteRenderer spriteRenderer;
+
+        private void Start()
+        {
+            spriteRenderer = GetComponent<SpriteRenderer>();
+            spriteRenderer.color = settings.LevelColor(0);
+        }
+
+        public void SetLevel(int currentLevel)
+        {
+            spriteRenderer.color = settings.LevelColor(currentLevel);
+        }
+    }
+}
