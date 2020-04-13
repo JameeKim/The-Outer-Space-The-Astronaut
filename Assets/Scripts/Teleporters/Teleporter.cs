e341e4f7cdda0a4cb868ea06efbdeda4ac0c7977
Jamee Kim <jameekim@nodaplife.me>
2020-04-12 21:36:52 -0400

    Implement teleporters (#42)

---
+++ Assets/Scripts/Teleporters/Teleporter.cs
@@ -0,0 +1,38 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Teleporters {
+    [DisallowMultipleComponent]
+    [RequireComponent(typeof(Collider2D))]
+    public class Teleporter : MonoBehaviour
+    {
+        [HideInPrefabAssets]
+        [SceneObjectsOnly]
+        [ValidateInput("MustBeSet")]
+        public Teleporter destination;
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            TeleportedEntity entity = other.GetComponent<TeleportedEntity>();
+
+            if (entity == null)
+                return; // only affect entities that are meant to be teleported
+
+            if (entity.IsSent)
+                return; // do not teleport this entity again if it is just sent over from another teleporter
+
+            entity.SetSent();
+            entity.RigidBody.position += (Vector2) (destination.transform.position - transform.position);
+        }
+
+#if UNITY_EDITOR
+        private bool MustBeSet(Teleporter value, ref string errorMsg)
+        {
+            bool isValid = value != null;
+            if (!isValid)
+                errorMsg = "The destination of the teleporter must be assigned";
+            return isValid;
+        }
+#endif
+    }
+}
