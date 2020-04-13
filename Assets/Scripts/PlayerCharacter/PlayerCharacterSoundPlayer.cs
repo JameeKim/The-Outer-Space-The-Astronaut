eedf4d766ddb8dafb0b6d7889d9362edd80e125a
Jamee Kim <jameekim@nodaplife.me>
2020-04-12 20:21:22 -0400

    Add sound effects

---
+++ Assets/Scripts/PlayerCharacter/PlayerCharacterSoundPlayer.cs
@@ -0,0 +1,25 @@
+﻿using Audio;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace PlayerCharacter {
+    [DisallowMultipleComponent]
+    public class PlayerCharacterSoundPlayer : MonoBehaviour
+    {
+        [SceneObjectsOnly]
+        public AudioRandomOneShotPlayer combatHitSoundPlayer;
+
+        [SceneObjectsOnly]
+        public AudioRandomOneShotPlayer hurtSoundPlayer;
+
+        public void PlayHitSound()
+        {
+            combatHitSoundPlayer.PlayRandom();
+        }
+
+        public void PlayHurtSound()
+        {
+            hurtSoundPlayer.PlayRandom();
+        }
+    }
+}
