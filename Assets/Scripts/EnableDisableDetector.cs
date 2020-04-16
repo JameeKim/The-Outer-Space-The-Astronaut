d609b2c7cde9eec26ebe21d3bc32a504c767844f
Jamee Kim <jameekim@nodaplife.me>
2020-04-16 00:13:25 -0400

    Implement UI sounds (#51)
    - Button clicks
    - Pop-up open and close

---
+++ Assets/Scripts/EnableDisableDetector.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+[DisallowMultipleComponent]
+public class EnableDisableDetector : MonoBehaviour
+{
+    public bool skipFirstTime;
+
+    public UnityEvent onEnable;
+
+    public UnityEvent onDisable;
+
+    private bool enabledOnce;
+
+    private bool disabledOnce;
+
+    private void OnEnable()
+    {
+        if (skipFirstTime && !enabledOnce)
+        {
+            enabledOnce = true;
+            return;
+        }
+
+        onEnable.Invoke();
+    }
+
+    private void OnDisable()
+    {
+        if (skipFirstTime && !disabledOnce)
+        {
+            disabledOnce = true;
+            return;
+        }
+
+        onDisable.Invoke();
+    }
+}
